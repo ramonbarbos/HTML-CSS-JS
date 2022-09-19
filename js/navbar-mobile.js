2cd1b5364532fe74d69caf522c0062e93515ccea
Ramon Barbosa <86085266+ramonbarbos@users.noreply.github.com>
2022-09-19 13:30:17 -0300

    att

--- js/navbar-mobile.js
+++ js/navbar-mobile.js
@@ -1,7 +1,9 @@
 const menu = document.querySelector(".menu");
 const navMenu = document.querySelector(".nav-menu");
+const busca = document.querySelector("input");
 
 menu.addEventListener("click", () =>{
     menu.classList.toggle('active');
     navMenu.classList.toggle('active');
+    busca.classList.toggle('active');
 });
