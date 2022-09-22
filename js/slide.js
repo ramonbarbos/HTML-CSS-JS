c559b6e71ee5078f2c5289ee22e9973e2bbb4020
Ramon Barbosa <86085266+ramonbarbos@users.noreply.github.com>
2022-09-22 11:54:06 -0300

    correção de bug

--- js/slide.js
+++ js/slide.js
@@ -2,16 +2,16 @@ let slides = document.querySelectorAll(".slide-container");
 let index = 0;
 
 function next(){
-    slides[index].classList.remove('active');
+    slides[index].classList.remove('ativo');
     index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
+    slides[index].classList.add('ativo');
 
 }
 
 function prev(){
-    slides[index].classList.remove('active');
+    slides[index].classList.remove('ativo');
     index = (index - 1 + slides.length) % slides.length;
-    slides[index].classList.add('active');
+    slides[index].classList.add('ativo');
 
 }
 setInterval(next, 5000);
